dc2a6f35d38a2bbdab95b4565f5d42f316042819
Altovskiy Maxim <altovskiy2006@gmail.com>
2025-03-14 00:49:12 +0200

    std::cin & std::cout support added

--- 14_Lab (BigInt Class)/main.cpp
+++ 14_Lab (BigInt Class)/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-using namespace std;
 
 class BigInt {
 private:
@@ -31,6 +30,32 @@ private:
         _data[n] = 0;
         _length = n;
     }
+
+    void CheckIsInegative() {
+        if (_length == 1 && _data[0] == '0') { _isNegative = false; }
+    }
+
+    size_t DigitLength(long long n) {
+        n = abs(n);
+        size_t tmp = n;
+        size_t length = 0;
+        while (tmp != 0) {
+            ++length;
+            tmp /= 10;
+        }
+        return length;
+    }
+
+    const char* FromNumberToString(long long n, size_t length = -1) {
+        if (length == -1) { length = DigitLength(n); }
+        char *new_arr = new char[length + 1];
+        for (int i = length - 1; i >= 0; --i) {
+            new_arr[i] = static_cast<char>((n % 10) + '0');
+            n /= 10;
+        }
+        new_arr[length] = 0;
+        return new_arr;
+    } 
 
     BigInt& Sum(const BigInt& other) {
         CheckCapacity(other._length + 1);
@@ -107,6 +132,8 @@ private:
         if (_length == 0) { _data[0] = '0'; _length++; }
         _data[_length] = 0;
 
+        if (_length == 1 && _data[0] == '0') { _isNegative = false; }
+
         return *this;
     }
 
@@ -146,13 +173,15 @@ private:
 
         _length = new_arr_len;
 
+        if (_length == 1 && _data[0] == '0') { _isNegative = false; }
+
         return *this;
     }
 
 public:
     BigInt() = default;
 
-    BigInt(size_t capacity) {
+    BigInt(size_t capacity, bool makeEmpty) {
         _capacity = capacity;
         _length = 0;
         _data = new char(_capacity);
@@ -181,6 +210,18 @@ public:
         _data[_length] = 0;
     }
 
+    BigInt (size_t number) {
+        _length = DigitLength(number);
+        const char *char_number = FromNumberToString(number, _length);
+        if (number < 0 ) { _isNegative = true; }
+        _capacity = _length + 8;
+        if (_data != nullptr) delete[] _data;
+        _data = new char[_capacity];
+        for (int i = _length - 1; i >= 0; i--) _data[_length - i - 1] = char_number[i];
+        _data[_length] = 0;
+        delete[] char_number;
+    }
+
     BigInt(const BigInt& other) {
         _isNegative = other._isNegative;
         _length = other._length;
@@ -204,14 +245,18 @@ public:
         if (_data != nullptr) delete[] _data;
     }
 
-    bool isNegative() { return _isNegative; }
+    bool isNegative() const { return _isNegative; }
 
-    size_t capacity() { return _capacity; }
+    size_t capacity() const { return _capacity; }
 
-    size_t length() { return _length; }
+    size_t length() const { return _length; }
 
-    void print() {
-        if (_length == 1 && _data[0] == '0') { _isNegative = false; }
+    const char& operator[] (size_t index) const {
+        if (0 <= index && index < _length) { return _data[index]; }
+        else { return _data[0]; }
+    }
+
+    void print() const {
         if (_isNegative) { fputc('-', stdout); }
         for (int i = _length - 1; i >= 0; i--) fputc(_data[i], stdout);
         fputc('\n', stdout);
@@ -241,20 +286,34 @@ public:
         }
     }
 
+    BigInt& operator+=(size_t number) {
+        BigInt other = number;
+        if (!_isNegative && !other._isNegative) { return this->Sum(other); }
+
+        else if (_isNegative && other._isNegative) { return this->Sum(other); }
+
+        else if (!_isNegative && other._isNegative) { 
+            if (*this >= other) { return this->Subtraction(other); }
+            else {
+                BigInt temp = *this;
+                *this = other;
+                return this->Subtraction(temp);
+            }
+        }
+
+        else {
+            if (*this >= other) { return this->Subtraction(other); }
+            else {
+                BigInt temp = *this;
+                *this = other;
+                return this->Subtraction(temp);
+             }
+        }
+    }
+
     BigInt operator+(const BigInt& other) {
         BigInt a = *this;
         a += other;
-        return a;
-    }
-
-    BigInt& operator*=(const BigInt& other) {
-        if (other._isNegative) { _isNegative = !_isNegative; }
-        return this->Multiplication(other);
-    }
-
-    BigInt operator*(const BigInt& other) {
-        BigInt a = *this;
-        a *= other;
         return a;
     }
 
@@ -289,31 +348,156 @@ public:
         else { return this->Sum(other); }
     }
 
-    bool operator<(const BigInt& other) {
-        if (_length < other._length) return true;
-        if (_length > other._length) return false;for (int i = _length - 1; i >= 0; i--) {
-            if (_data[i] < other._data[i]) return true;
-            if (_data[i] > other._data[i]) return false;
+    BigInt& operator-=(size_t number) {
+        BigInt other = number;
+        if (!_isNegative && !other._isNegative) { 
+            if (*this >= other) { return this->Subtraction(other); }
+            else {
+                BigInt temp = *this;
+                *this = other;
+                _isNegative = true;
+               return this->Subtraction(temp);
+            }
         }
-        return false;
+        
+        else if (_isNegative && other._isNegative) { 
+            if (*this >= other) { 
+                _isNegative = true;
+                return this->Subtraction(other); 
+            }
+            else {
+                BigInt temp = *this;
+                *this = other;
+                _isNegative = false;
+               return this->Subtraction(temp);
+            }
+        }
+
+        else if (!_isNegative && other._isNegative) { 
+            return this->Sum(other);
+        }
+
+        else { return this->Sum(other); }
+    }
+
+    BigInt operator-(const BigInt other) {
+        BigInt a = *this;
+        a -= other;
+        return a;
+    }
+
+    BigInt& operator*=(const BigInt& other) {
+        if (other._isNegative) { _isNegative = !_isNegative; }
+        return this->Multiplication(other);
+    }
+
+    BigInt& operator*=(size_t number) {
+        BigInt other = number;
+        if (other._isNegative) { _isNegative = !_isNegative; }
+        return this->Multiplication(other);
+    }
+
+    BigInt operator*(const BigInt& other) {
+        BigInt a = *this;
+        a *= other;
+        return a;
+    }
+
+    BigInt operator++() { 
+        BigInt copy = *this;
+        *this += 1;
+        return copy;
+    }
+
+    BigInt& operator++(int) {
+        *this += 1;
+        return *this;
+    }
+
+    BigInt operator--() {
+        BigInt copy = *this;
+        *this -= 1;
+        return copy;
+    }
+
+    BigInt& operator--(int) {
+        *this -= 1;
+        return *this;
+    }
+
+    bool operator<(const BigInt& other) {
+        bool result;
+        if (_length < other._length) result = true;
+        else if (_length > other._length) result = false;
+        else {
+            for (int i = _length - 1; i >= 0; i--) {
+                if (_data[i] < other._data[i]) { result = true; break; }
+                if (_data[i] > other._data[i]) { result = false; break; }
+            }
+        }
+        if (other._isNegative) { result = !result; }
+        return result;
+    }
+
+    bool operator<(size_t number) {
+        BigInt other = number;
+        bool result;
+        if (_length < other._length) result = true;
+        else if (_length > other._length) result = false;
+        else {
+            for (int i = _length - 1; i >= 0; i--) {
+                if (_data[i] < other._data[i]) { result = true; break; }
+                if (_data[i] > other._data[i]) { result = false; break; }
+            }
+        }
+        if (other._isNegative) { result = !result; }
+        return result;
     }
 
     bool operator>=(const BigInt& other) {
         return !(*this < other);
     }
 
-    bool operator>(const BigInt& other) {
-        if (_length < other._length) return false;
-        if (_length > other._length) return true;
+    bool operator>=(size_t number) {
+        BigInt other = number;
+        return !(*this < other);
+    }
 
-        for (int i = _length - 1; i >= 0; i--) {
-            if (_data[i] < other._data[i]) return false;
-            if (_data[i] > other._data[i]) return true;
+    bool operator>(const BigInt& other) {
+        bool result;
+        if (_length < other._length) result = false;
+        else if (_length > other._length) result = true;
+        else {
+            for (int i = _length - 1; i >= 0; i--) {
+                if (_data[i] < other._data[i]) { result = false; break; }
+                if (_data[i] > other._data[i]) { result = true; break; }
+            }
         }
-        return false;
+        if (other._isNegative) { result = !result; }
+        return result;
+    }
+
+    bool operator>(size_t number) {
+        BigInt other = number;
+        bool result;
+        if (_length < other._length) result = false;
+        else if (_length > other._length) result = true;
+        else {
+            for (int i = _length - 1; i >= 0; i--) {
+                if (_data[i] < other._data[i]) { result = false; break; }
+                if (_data[i] > other._data[i]) { result = true; break; }
+            }
+        }
+        if (other._isNegative) { result = !result; }
+        return result;
     }
 
     bool operator<=(const BigInt& other) {
+        return !(*this > other);
+    }
+
+    bool operator<=(size_t number) {
+        BigInt other = number;
         return !(*this > other);
     }
 
@@ -326,181 +510,79 @@ public:
         return true;
     }
 
+    bool operator==(size_t number) {
+        BigInt other = number;
+        if (_length != other._length) return false;
+
+        for (int i = 0; i < _length; i++) {
+            if (_data[i] != other._data[i]) return false;
+        }
+        return true;
+    }
+
     bool operator!=(const BigInt& other) {
+        return !(*this == other);
+    }
+
+    bool operator!=(size_t number) {
+        BigInt other = number;
         return !(*this == other);
     }
 };
 
+BigInt operator+(size_t number, const BigInt& other) {
+    BigInt a = number;
+    a += other;
+    return a;
+}
+
+BigInt operator-(size_t number, const BigInt& other) {
+    BigInt a = number;
+    a -= other;
+    return a;
+}
+
+BigInt operator*(size_t number, const BigInt& other) {
+    BigInt a = number;
+    a *= other;
+    return a;
+}
+
+std::istream& operator>>(std::istream& in, BigInt& other) {
+    std::string s;
+    in >> s;
+    other = BigInt(s.data());
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const BigInt& other) {
+    std::string s = "";
+    if (other.isNegative()) { s += '-'; }
+    for (int i = other.length() - 1; i >= 0; i--) s += other[i];
+    out << s;
+    return out;
+}
+
 int main() {
-    BigInt a("100");
-    BigInt b = "10";
-    a *= b;
-    cout << a.length() << endl;
+    BigInt a(10);
+    BigInt b = 50;
+    BigInt c = "-12345";
+
+    a++;
+    ++a;
     a.print();
+    
+    b--;
+    --b;
+    b.print();
+    
+    c += b;
+    c -= a;
+    c.print();
+
+    std::cin >> a >> b;
+    std::cout << "a + b = " << a + b << "\na - b = " << a - b << "\na * b = " << a * b << std::endl;
+    std::cout << (a < b) << " " << (a > b) <<  " " << (a != b) << std::endl;
+   
+    return 0;
 }
-
-/*
-for (int i = 0; i < _length; i++) {
-
-}
-
-if (!_isNegative) { // Работаем с положительн числом
-
-            
-            if (!other._isNegative) { return this->Subtraction(other); }
-            else { return this->Sum(other); }
-            
-        }
-        // Здесь работаем с негативным числом
-        else if (_length >= other._length) { // Если длина первого больше или равна длине второго
-            if (!other._isNegative) { return this->Subtraction(other); }
-            else { return this->Sum(other); }
-        }
-        else { // Если длина первого меньше длины второго
-            if (!other._isNegative) { return this->Sum(other); }
-            //else { return }
-        }
-
-
-BigInt& operator*=(const BigInt& other) {
-            short new_arr_len = _length + other._length;
-            CheckCapacity(new_arr_len + 1);
-            char *new_arr = new char[_capacity];
-
-            for (int i = 0; i < new_arr_len + 1; i++) { new_arr[i] = '0'; }
-
-            short carry, sum_carry = 0;
-            for (int i = 0; i < other._length; i++) {
-                //carry = 0; sum_carry = 0;
-                for (int j = 0; j < _length; j++) {
-                    short tmp = static_cast<short>((_data[j] - '0') * (other._data[i] - '0')) + carry + sum_carry;
-                    sum_carry = 0;
-                    carry = tmp / 10;
-                    if (tmp > 9) {
-                        tmp = tmp % 10;
-                    }
-                    short sum_tmp = static_cast<short>(new_arr[i + j] - '0') + tmp;
-                    sum_carry = sum_tmp / 10;
-                    if (sum_tmp > 9) {
-                        sum_tmp = sum_tmp % 10;
-                    }
-
-                    new_arr[i + j] = static_cast<char>(sum_tmp + '0');
-                }
-            }
-            if (new_arr[new_arr_len - 1] == '0') { new_arr_len--; }
-            new_arr[new_arr_len] = 0;
-
-            char *tmp_arr = _data;
-            _data = new_arr;
-            new_arr = tmp_arr;
-            delete[] new_arr;
-
-            _length = new_arr_len;
-
-            return *this;
-        }BigInt& operator+=(const BigInt& other) {
-            CheckCapacity(other._length + 1); // потом попробовать убрать единицу
-            _data[_length] = '0';
-            short carry = 0;
-            bool flag = true;
-            if (other._length > _length) { _length = other._length; flag = false; }
-            for (int i = 0; i < other._length; i++) {
-                short tmp = static_cast<short>((_data[i] - '0') + (other._data[i] - '0')) + carry;
-                carry = 0;
-                if (9 < tmp && tmp < 100) { carry = tmp / 10; }
-                _data[i] = static_cast<char>((tmp % 10) + '0');
-            }
-            if (flag) {
-                for (int i = other._length; i < _length + 1; i++) {
-                    short tmp = static_cast<short>(_data[i] - '0') + carry;
-                    carry = 0;
-                    if (9 < tmp && tmp < 100) { carry = tmp / 10; }
-                    _data[i] = static_cast<char>((tmp % 10) + '0');
-                }
-                if (_data[_length] != '0') { _length++; }
-            }
-
-            _data[_length] = 0;
-            return *this;
-        }
-
-
-*/
-
-/*
-BigInt& operator+=(const BigInt& other) {
-    short tmp;
-    if (other._length > _length) {
-        if (other._length >= _capacity) {
-            ExtendCapacity(other._length - _capacity + 8);
-        }
-    }
-    for (int i = 0; i < other._length; i++) {
-        if (i >= _length) {
-            _length++;
-            _data[i + 1] = 0;
-            _data[i] = '0';
-        }
-        tmp = static_cast<short>((_data[i] - '0') + (other._data[i] - '0'));
-        if (tmp > 9) {
-            if (CheckCapacity(i + 1)) {
-                _data[i + 1] = '0';
-                _length++;
-            }
-            _data[i + 1]++;
-            tmp -= 10;
-        }
-        _data[i] = static_cast<char>(tmp + '0');
-    }
-    return *this;
-}
-*/
-/*
-        BigInt& operator+=(const BigInt& other) {
-            for (int i = 0; i < other._length; i++) {
-                AddDigits(_data, other._data[i], i, *this);
-            }
-            return *this;
-        }
-
-        void AddDigits(char *data, char c, size_t index, BigInt& current) {
-            short tmp = static_cast<short>((data[index] - '0') + (c - '0'));
-            if (tmp > 9) {
-                short remainder = tmp % 10;
-                AddDigits(data, static_cast<short>((tmp / 10) + '0'), index + 1, current);
-                current._length++;
-            }
-            data[index] = static_cast<short>(tmp + '0');
-        }
-*/
-/*
-        BigInt& operator+=(const BigInt& other) {
-            char *tmp_arr = new char[other._length + 8];
-            tmp_arr[0] = '0';
-            for (int i = 0; i < _length; i++) { // Складываем все цифры, пока не закончится длина числа к которому прибалвяем
-                short tmp = static_cast<short>((_data[i] - '0') + (other._data[i] - '0'));
-                if (9 < tmp && tmp < 100) { tmp_arr[i + 1] = static_cast<char>((tmp / 10) + '0'); }
-                _data[i] = static_cast<short>((tmp % 10) + '0');
-            }
-            tmp_arr[_length + 1] = 0;
-            if (other._length > _length) { // Добавляем непоместившиеся цифры в число
-                CheckCapacity(other._length);
-                for (int i = _length; i < other._length; i++) {
-                    _length++;
-                    _data[i] = other._data[i];
-                }
-                _data[_length] = 0;
-            }
-            short carry = 0;
-            for (int i = 1; i < _length; i++) { // Складываем цифры из числа с цифрами из массива tmp_arr
-                short tmp = static_cast<short>((_data[i] - '0') + (tmp_arr[i] - '0')) + carry;
-                carry = 0;
-                if (9 < tmp && tmp < 100) {
-                    carry = tmp / 10;
-                }
-                _data[i] = static_cast<char>((tmp % 10) + '0');
-            }
-            return *this;
-        }
-        */
